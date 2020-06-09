5a581456653eb17e8fd4c02266ac8bc11bca2f67
Antoine Pronnier <antoine.pronnier@epitech.eu>
2020-06-09 21:49:52 +0200

    remove all

--- main.js
+++
@@ -1,5 +0,0 @@
-const Discord = new (require('./modules/DiscordClient'))();
-const ConsoleCmds = new (require('./modules/ConsoleCommands'))();
-const TwitchApi = require('./modules/TwitchApi');
-
-setInterval(TwitchApi.statusLive, 10000, Discord);
