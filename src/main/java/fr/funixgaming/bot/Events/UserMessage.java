852f587b40fa56b236cb8573cc51264d3c978c5c
Antoine Pronnier <antoine.pronnier@epitech.eu>
2020-06-10 14:28:05 +0200

    adding log system

--- src/main/java/fr/funixgaming/bot/Events/UserMessage.java
+++ src/main/java/fr/funixgaming/bot/Events/UserMessage.java
@@ -1,5 +1,9 @@
 package fr.funixgaming.bot.Events;
 
+import fr.funixgaming.bot.Utils.Log;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
@@ -7,5 +11,10 @@ public class UserMessage extends ListenerAdapter {
 
     public void onGuildMessageReceived(GuildMessageReceivedEvent e) {
         if (e.getAuthor().isBot()) return;
+        User user = e.getAuthor();
+        TextChannel channel = e.getChannel();
+        Message message = e.getMessage();
+
+        Log.logMessage(user, channel, message);
     }
 }
--- src/main/java/fr/funixgaming/bot/Utils/Log.java
+++ src/main/java/fr/funixgaming/bot/Utils/Log.java
@@ -1,4 +1,48 @@
 package fr.funixgaming.bot.Utils;
 
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.entities.User;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Log {
+
+    private static final File logFolder = new File("data", "logs");
+
+    public static void logMessage(User user, TextChannel textChannel, Message message) {
+        new Thread(() -> {
+            DateFormat dateFormat = new SimpleDateFormat("d.MM.yyyy");
+            DateFormat timeFormat = new SimpleDateFormat("HH:mm");
+            Date date = new Date();
+            String dateFormated = dateFormat.format(date);
+            String time = timeFormat.format(date);
+
+            if (!logFolder.exists() && !logFolder.mkdirs()) {
+                System.err.println("Error while creating data and logs folder.");
+                return;
+            }
+
+            try {
+                File logFile = new File(logFolder, dateFormated + ".log");
+                if (!logFile.exists() && !logFile.createNewFile())
+                    throw new IOException("Could not create file");
+                BufferedWriter fileWriter = new BufferedWriter(new FileWriter(logFile, true));
+                String log = time + " > " + user.getAsTag() + " [" + textChannel.getName() + "] " + message.getContentRaw() + "\n";
+                System.out.print(log);
+                fileWriter.write(log);
+                fileWriter.close();
+            } catch (IOException e) {
+                System.err.println("Error on today log file.");
+                e.printStackTrace();
+            }
+        }).start();
+    }
+
 }
